54ee8bb65e1bdcf8e4dfe85d5138e298f4ed43ac
Maxim <maxillaenpozdnyakov@gmail.com>
2022-10-21 11:34:31 +0300

    Creat class List and Node for singly linked list

---
+++ лабы/laba3/laba3/List.cs
@@ -0,0 +1,43 @@
+﻿namespace laba3
+{
+    internal class List
+    {
+        Node tail;
+        Node head;
+        int length;
+        public List()
+        {
+            tail = null;
+            head = null;
+            length = 0;
+        }
+        public Node Head
+        {
+            get => head;
+        }
+        public void AddNode(string info)
+        {
+            Node node = new Node();
+            node.Info = info;
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.Next = node;
+            }
+            tail = node;
+            length++;
+        }
+        public void ShowInfo()
+        {
+            Node node = head;
+            while (node != null)
+            {
+                Console.WriteLine(node.Info);
+                node = node.Next;
+            }
+        }
+    }
+}
---
+++ лабы/laba3/laba3/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace laba3
+{
+    class Node
+    {
+        string inf;
+        Node next;
+        public string Info
+        {
+            get => inf;
+            set => inf = value;
+        }
+        public Node Next
+        {
+            get => next;
+            set => next = value;
+        }
+    }
+}

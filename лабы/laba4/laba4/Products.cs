c7a3a047aab4d7491323e4f8eec665d5d334b67f
Maxim <maxillaenpozdnyakov@gmail.com>
2022-10-30 23:33:00 +0300

    created abstact class Profuct

---
+++ лабы/laba4/laba4/Products.cs
@@ -0,0 +1,36 @@
+﻿namespace laba4
+{
+    abstract class Product
+    {
+        protected int _amount;
+        protected int _price;
+
+        public Product(int amount, int price)
+        {
+            _amount = amount;
+            _price = price;
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("----Информация о товаре----");
+            Console.WriteLine($"Количество: {_amount}");
+            Console.WriteLine($"Цена: {_price} руб.");
+            return "\0";
+        }
+    }
+
+
+}

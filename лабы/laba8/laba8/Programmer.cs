a26d2756bd804912cecf6705b4930e00fa23d26b
Maxim <maxillaenpozdnyakov@gmail.com>
2022-11-28 12:52:33 +0300

    creat class Programmer

---
+++ лабы/laba8/laba8/Programmer.cs
@@ -0,0 +1,26 @@
+﻿namespace laba8
+{
+    internal class Programmer
+    {
+        public delegate void Action(List<string> list);
+        public event Action delete;
+        public event Action mutate;
+
+        public void Delete(List<string> list)
+        {
+            Console.Write("Ведите номер элемента, который хотите удалить(начиная с 0): ");
+            int num = int.Parse(Console.ReadLine());
+            list.RemoveAt(num);
+            delete?.Invoke(list);
+        }
+
+        public void Mutate(List<string> list)
+        {
+            Random random = new Random();
+            List<string> NewList = list.OrderBy(item => random.Next()).ToList();
+            list.Clear();
+            list.AddRange(NewList);
+            mutate?.Invoke(list);
+        }
+    }
+}

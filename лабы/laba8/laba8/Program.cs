866f2efad08729cc3072dfb6dd1d7224c05eb7cd
Maxim <maxillaenpozdnyakov@gmail.com>
2022-12-02 10:58:34 +0300

    add delegates and events

--- лабы/laba8/laba8/Program.cs
+++ лабы/laba8/laba8/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Programmer.Action Language;
+            Programmer programmer = new Programmer();
+            List<string> LP = new List<string> { "Ruby", "C#", "Kotlin", "Pascal", "Python", "GoLang", "VisualBasic", "Dart", "JS", "Java" };
+
+            Console.Write("Список: ");
+            foreach (string item in LP)
+            {
+                Console.Write(item + "   ");
+            }
+            Console.WriteLine();
+
+            programmer.delete += list =>
+            {
+                Console.Write("Измененный: ");
+                foreach (string item in LP)
+                {
+                    Console.Write(item + "   ");
+                }
+                Console.WriteLine();
+            };
+
+            programmer.mutate += list =>
+            {
+                Console.Write("Перестановка: ");
+                foreach (string item in LP)
+                {
+                    Console.Write(item + "   ");
+                }
+                Console.WriteLine();
+            };
+
+            Language = programmer.Delete;
+            Language += programmer.Mutate;
+            Language += programmer.Mutate;
+            Language += programmer.Mutate;
+            Language(LP);
         }
     }
 }

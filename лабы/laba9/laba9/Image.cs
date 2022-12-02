c9f7337f5c43febac6b144fa0b4f8302254d139a
Maxim <maxillaenpozdnyakov@gmail.com>
2022-12-02 12:55:01 +0300

    creat class Image

---
+++ лабы/laba9/laba9/Image.cs
@@ -0,0 +1,15 @@
+﻿namespace laba9
+{
+    internal class Image
+    {
+        public string _type;
+        public Image(string type)
+        {
+            type = _type;
+        }
+        public override string ToString()
+        {
+            return _type;
+        }
+    }
+}

6891c08e7c4b8abf68d7a250b7eea766d90b0aeb
Maxim <maxillaenpozdnyakov@gmail.com>
2022-12-02 13:25:17 +0300

    add interface ISet and its realization

---
+++ лабы/laba9/laba9/MyCollection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace laba9
+{
+    public interface ISet<T>
+    {
+        void Add(T item);
+        void Delete(T item);
+        void Search(T item);
+        void ShowCollection();
+    }
+
+    internal class MyCollection<T> : ISet<T>
+    {
+        private ArrayList items = new ArrayList();
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Delete(T item)
+        {
+            items.Remove(item);
+        }
+        public void Search(T item)
+        {
+            if (items.Contains(item))
+            {
+                Console.WriteLine($"{item} найден");
+            }
+            else
+            {
+                Console.WriteLine($"{item} не найден");
+            }
+        }
+        public void ShowCollection()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public IEnumerator GetEnumerator() => items.GetEnumerator();
+    }
+}

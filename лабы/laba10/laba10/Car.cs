46ab29a6de5c62a1ab0204e6d69eab9a29b4cc95
Maxim <maxillaenpozdnyakov@gmail.com>
2022-12-09 15:55:35 +0300

    creat List with class Car

---
+++ лабы/laba10/laba10/Car.cs
@@ -0,0 +1,113 @@
+﻿namespace laba10
+{
+    internal class Car
+    {
+        const int a = 0;
+        static private int? _counter;
+        private readonly int _id;
+        private string? _brand;
+        private string? _color;
+        private int? _price;
+        private int? _realeseYear;
+        private int? _registrationNumber;
+
+        public Car()
+        {
+            _counter++;
+        }
+
+        public Car(string? color, string? brand, int? realeseYear) : this(brand)
+        {
+            _counter++;
+            _id = GetHashCode();
+            _realeseYear = realeseYear;
+        }
+
+        private Car(string? brand)
+        {
+            _brand = brand;
+        }
+
+        public Car(string? brand = null, int? realeseYear = null, string? color = null, int? price = null, int? registrationNumber = null)
+        {
+            _brand = brand;
+            _id = GetHashCode();
+            RealeseYear = (int)realeseYear;
+            _color = color;
+            _price = price;
+            _registrationNumber = registrationNumber;
+        }
+
+        public int Counter
+        {
+            get { return (int)_counter; }
+        }
+
+        public int ID
+        {
+            get { return (int)_id; }
+        }
+
+        public string Brand
+        {
+            get { return (string)_brand; }
+            set { _brand = value; }
+        }
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            Random rand = new Random();
+            for (int i = 0; i < _brand.Length; i++)
+            {
+                hash += _brand[i];
+            }
+            hash *= rand.Next(1000, 999999);
+            return (int)Math.Abs(hash * _brand.Length);
+        }
+        public int RealeseYear
+        {
+            get { return (int)_realeseYear; }
+            set{ _realeseYear = value; }
+        }
+
+        public string Color
+        {
+            get { return (string)_color; }
+            set { _color = value; }
+        }
+
+        public int Price
+        {
+            get { return (int)_price; }
+            set { _price = value; }
+        }
+
+        public int RegistrationNumber
+        {
+            get { return (int)_registrationNumber; }
+            set { _registrationNumber = value; }
+        }
+
+        public void GetId(out int id)
+        {
+            id = (int)_id;
+        } // out
+
+        public void GetGroup(ref int price)
+        {
+            price = (int)_price;
+        } // ref
+
+        public void GetInformationOfCar()
+        {
+            Console.WriteLine("Информация о машине.");
+            Console.WriteLine($"ID машины: {_id}");
+            Console.WriteLine($"Марка машины: {_brand}");
+            Console.WriteLine($"Цвет машины: {_color}");
+            Console.WriteLine($"Год выпуска: {_realeseYear}");
+            Console.WriteLine($"Регистрационный номер: {_registrationNumber}");
+            Console.WriteLine($"Цена: {_price}$");
+            Console.WriteLine(new String('-', 50));
+        }
+    }
+}
--- лабы/laba10/laba10/Program.cs
+++ лабы/laba10/laba10/Program.cs
@@ -52,6 +52,38 @@
             }
             Console.WriteLine();
 
+            string brand = "AAA";
+            int years = 15;
+            string color = "white";
+
+            List<Car> list = new List<Car>()
+            {
+                new Car("AAA", 2010, "black", 20000, 123456789),
+                new Car("BBB", 2000, "black", 19000, 123456789),
+                new Car("CCC", 2000, "white", 18000, 123456789),
+                new Car("DDD", 1990, "yellow", 17000, 123456789),
+                new Car("AAA", 2007, "white", 19000, 123456789),
+                new Car("BBB", 1999, "blue", 5000, 123456789),
+                new Car("XXX", 2022, "white", 100, 123456789),
+                new Car("YYY", 1950, "black", 190000, 123456789),
+                new Car("ZZZ", 2000, "black", 19000, 123456789),
+                new Car("AAA", 2021, "white", 19000, 123456789),
+            };
+
+            var task1 = from car in list where car.Brand == brand select car;
+            var task2 = list.Where(car => car.Brand == brand && 2022 - car.RealeseYear >= 15);
+            var task3 = list.Where(car => car.Color == color && car.Price <= 20000);
+            var task4 = list.Min(car => car.RealeseYear);
+            var task5 = list.OrderByDescending(car => car.RealeseYear).Take(5);
+            var task6 = list.OrderByDescending(car => car.Price);
+            var task7 = list.Union(task3).OrderBy(car => car.RealeseYear).Where(car => car.Color == "white").Skip(1).Max();
+            var task8 = list.Join(task2, car1 => car1.Brand, car2 => car2.Brand, (car1, car2) => new {price = car1.Price, color = car2.Color});
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
         }
     }
 }

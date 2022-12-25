c277109cdcc977532a15d4b05173e9a4a3b9d4ea
Maxim <maxillaenpozdnyakov@gmail.com>
2022-12-26 01:53:08 +0300

    add class PMIFileInfo

---
+++ лабы/laba12/laba12/PMIFileInfo.cs
@@ -0,0 +1,44 @@
+﻿namespace laba12
+{
+    internal static class PMIFileInfo
+    {
+        private static string _path = PMILog.Path;
+        private static string _pathToFile;
+        public static string PathToFile
+        {
+            set
+            {
+                _pathToFile = value;
+            }
+        }
+        public static void WriteFullPath()
+        {
+            if (_path == null || _pathToFile == null || !File.Exists(_pathToFile))
+            {
+                throw new Exception("No path to file.");
+            }
+            File.AppendAllText(_path, $"{DateTime.Now}\n");
+            File.AppendAllText(_path, $"Полный путь к файлу: {(new FileInfo(_pathToFile)).DirectoryName} \n");
+        }
+        public static void WriteFulInf()
+        {
+            if (_path == null || _pathToFile == null || !File.Exists(_pathToFile))
+            {
+                throw new Exception("No path to file.");
+            }
+            File.AppendAllText(_path, $"{DateTime.Now}\n");
+            File.AppendAllText(_path, $"Размер: {(new FileInfo(_pathToFile)).Length / Math.Pow(10, 6)} МБ, " +
+                $"Расширение: {(new FileInfo(_pathToFile)).Extension}, Имя: {(new FileInfo(_pathToFile)).Name} \n");
+        }
+        public static void WritrDopInf()
+        {
+            if (_path == null || _pathToFile == null || !File.Exists(_pathToFile))
+            {
+                throw new Exception("No path to file.");
+            }
+            File.AppendAllText(_path, $"{DateTime.Now}\n");
+            File.AppendAllText(_path, $"Дата создания: {(new FileInfo(_pathToFile)).CreationTime} " +
+                $"Дата изменения: {(new FileInfo(_pathToFile)).LastWriteTime} \n");
+        }
+    }
+}

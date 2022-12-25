5debddfb57d3c65933ee00d61d5a5fe2b6f07508
Maxim <maxillaenpozdnyakov@gmail.com>
2022-12-26 01:34:32 +0300

    add class PMILog

---
+++ лабы/laba12/laba12/PMILog.cs
@@ -0,0 +1,16 @@
+﻿namespace laba12
+{
+    internal static class PMILog
+    {
+        private static string _path;
+        public static string Path
+        {
+            set { _path = value; }
+            get
+            {
+                File.AppendAllText(_path, $"{DateTime.Now}\n");
+                return _path;
+            }
+        }
+    }
+}

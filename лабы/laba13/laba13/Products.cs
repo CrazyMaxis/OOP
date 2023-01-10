a169a9b175d954de249c4a3ce5d2d6a7c4e77899
Maxim <maxillaenpozdnyakov@gmail.com>
2023-01-10 09:27:30 +0300

    add class product and pastry from laba4

---
+++ лабы/laba13/laba13/Products.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba13
+{
+    [Serializable]
+    internal class Product
+    {
+        protected int _price;
+
+        public Product(int price)
+        {
+            _price = price;
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("----Информация о товаре----");
+            Console.WriteLine($"Цена: {_price} руб.");
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+
+    [Serializable]
+    internal class Pastry : Product
+    {
+        protected int _weight;
+        [NonSerialized]
+        protected string _factory;
+
+        public Pastry(int price, int weight, string factory) : base(price)
+        {
+            _weight = weight;
+            _factory = factory;
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public string Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+    }
+}

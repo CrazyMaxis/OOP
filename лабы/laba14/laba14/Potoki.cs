be3c7964c460d1889beed95c34b16db06c5e785b
Maxim <maxillaenpozdnyakov@gmail.com>
2023-01-10 11:54:22 +0300

    print all processes

---
+++ лабы/laba14/laba14/Potoki.cs
@@ -0,0 +1,93 @@
+﻿namespace laba14
+{
+    internal class Potoki
+    {
+        public static void printNumb(object n)
+        {
+            int n2 = (int)n;
+            for (int j = 1; j <= n2; j++)
+            {
+                Console.WriteLine(j);
+                Thread.Sleep(5);
+            }
+        }
+        public static void DoItConsistently()
+        {
+            object locker = new object();
+
+            Thread Nech_Thread = new Thread(new ThreadStart(Print_Nech));
+            Thread Ch_Thread = new Thread(new ThreadStart(Print_Ch));
+            Nech_Thread.Start();
+            Ch_Thread.Start();
+
+            void Print_Nech()
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            Console.WriteLine(i + " неч");
+                            Thread.Sleep(250);
+                        }
+                    }
+                }
+            }
+
+            void Print_Ch()
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine(i + " чет");
+                            Thread.Sleep(500);
+                        }
+                    }
+                }
+            }
+        }
+        public static void DoItOneByOne()
+        {
+            Mutex mutex = new Mutex();  //организ. критич секцию для неск запросов
+
+            Thread Nech_Thread = new Thread(new ThreadStart(Print_Nech));
+            Thread Ch_Thread = new Thread(new ThreadStart(Print_Ch));
+            Nech_Thread.Start();
+            Ch_Thread.Start();
+
+            void Print_Nech()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    mutex.WaitOne();    //вход в крит. секцию
+
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i + " неч");
+                        Thread.Sleep(250);
+                    }
+                    mutex.ReleaseMutex();   //выход из нее
+                }
+            }
+
+            void Print_Ch()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    mutex.WaitOne();
+
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i + " чет");
+                        Thread.Sleep(500);
+                    }
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
---
+++ лабы/laba14/laba14/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+
+namespace laba14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Инфо Процессов:");
+            var allProcesses = Process.GetProcesses();
+            Console.Write("{0,-10}", "ID:");
+            Console.Write("{0,-40}", "Process Name:");
+            Console.Write("{0,-10}", "Priority:\n");
+            foreach (var process in allProcesses)
+            {
+                Console.Write("{0,-10}", $"{process.Id}");
+                Console.Write("{0,-40}", $"{process.ProcessName}");
+                Console.Write("{0,-10}", $"{process.BasePriority}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n\n\nИнфо домена:");
+            var thisAppDomain = Thread.GetDomain();
+
+            Console.WriteLine($"Name:  {thisAppDomain.FriendlyName}");
+            Console.WriteLine($"Setup Information:  {thisAppDomain.SetupInformation.ToString()}");
+            Console.WriteLine("Assemblies:");
+
+            foreach (var item in thisAppDomain.GetAssemblies())
+            {
+                Console.WriteLine("    " + item.FullName.ToString());
+            }
+
+            //AppDomain domen = AppDomain.CreateDomain("Domen");
+            //domen.Load("Assembly");
+            //AppDomain.Unload(domen);
+
+            Thread simpleThread = new Thread(new ParameterizedThreadStart(Potoki.printNumb));
+            simpleThread.Start(8);
+            Console.WriteLine("Приоритет: {0}", $"{simpleThread.Priority}");
+            Console.WriteLine("Имя: {0}", $"{simpleThread.Name}");
+            Console.WriteLine("Числовой ID: {0}", $"{simpleThread.ManagedThreadId}");
+            simpleThread.Join();
+
+
+            Console.WriteLine("СНАЧАЛА НЕЧЕТНЫЕ, ПОТОМ ЧЕТНЫЕ:");
+            Potoki.DoItConsistently();
+            Thread.Sleep(4000);
+
+            Console.WriteLine("\nОДНО НЕЧЕТНОЕ, ОДНО ЧЕТНОЕ:");
+            Potoki.DoItOneByOne();
+            Thread.Sleep(4000);
+
+            TimerCallback timerCallback = new TimerCallback(WhatTimeIsIt);
+            Timer timer = new Timer(timerCallback, null, 0, 1000);
+            Thread.Sleep(5000);
+
+            void WhatTimeIsIt(object obj)
+            {
+                Console.WriteLine($"It's {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
